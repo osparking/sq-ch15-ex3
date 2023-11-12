a46df584c34f32cc9445dea7ae31a7d1ca1c7173
osparking <jbpark03@gmail.com>
2023-11-12 09:59:03 +0900

    종속: web, h2, data-jdbc, lombok

---
+++ src/main/java/sqch15ex3/SqCh15Ex3Application.java
@@ -0,0 +1,13 @@
+package sqch15ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh15Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh15Ex3Application.class, args);
+	}
+
+}
---
+++ src/test/java/sqch15ex3/SqCh15Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sqch15ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh15Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
