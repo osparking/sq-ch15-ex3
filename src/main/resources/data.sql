8d215b6f18d81720805452cd8aadc4d2bde94c84
osparking <jbpark03@gmail.com>
2023-11-12 15:33:26 +0900

    스키마 및 자료 sql 파일, h2 DB 콘솔 접근 허용

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO account VALUES(1, '철수', 10000);
+INSERT INTO account VALUES(2, '영자', 5000);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS account;
+CREATE TABLE account(ID INT PRIMARY KEY, NAME VARCHAR(255),
+    amount numeric(20, 0));
