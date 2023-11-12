506396de8d385521d9125fcbd079856d06c37c8c
osparking <jbpark03@gmail.com>
2023-11-12 15:49:55 +0900

    봄 자료 JDBC 활용 h2 디비 계좌 테이블 메소드

---
+++ src/main/java/sqch15ex3/repository/AccountRepository.java
@@ -0,0 +1,27 @@
+package sqch15ex3.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import sqch15ex3.model.Account;
+
+public interface AccountRepository extends CrudRepository<Account, Long>{
+	// 계좌 정보 검색 메소드
+	@Query("select * from account where id=:id")
+	public Optional<Account> findAccountById(Long id);
+	
+	// 계좌 목록 읽는 메소드
+	@Query("select * from account where id=:id")
+	public List<Account> findAccounts();
+	
+	// 계좌 금액 갱신 메소드
+	@Query("update account set amount=:amount where id=:id")
+	@Modifying
+	public void updateAccountAmount(BigDecimal amount, Long id);
+
+}
